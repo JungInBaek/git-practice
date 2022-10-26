f72d9439d577e67491dbabc246ce7aa50744def0
JungInBaek <44565979+JungInBaek@users.noreply.github.com>
2022-10-26 13:30:56 +0900

    git 연습 프로젝트 생성

---
+++ src/practice/Page2.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Page2 {
+	public void open() {
+		JFrame f = new JFrame();
+		f.setSize(500, 500);    
+		f.setVisible(true);
+	}
+}
---
+++ src/practice/Page4.java
@@ -0,0 +1,11 @@
+package practice;
+
+import javax.swing.JFrame;
+
+public class Page4 {
+	public void open() {
+		JFrame f = new JFrame();
+		f.setSize(500, 500);
+		f.setVisible(true);
+	}
+}
