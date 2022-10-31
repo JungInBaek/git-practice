2eeed7776ed00bad41fb74e156918ae4b7399058
Kwonts <power110022@naver.com>
2022-10-31 17:39:03 +0900

    MemberDAO syso제거

--- src/dao/MemberDAO.java
+++ src/dao/MemberDAO.java
@@ -10,15 +10,15 @@ import constant.Role;
 import vo.MemberVO;
 
 public class MemberDAO {
-	
+
 	private String dbUrl = "jdbc:oracle:thin:@localhost:1521:xe";
 	private String dbId = "cafe";
 	private String dbPw = "1234";
-	
+
 	// member 추가
 	public void add(MemberVO vo) {
 		String sql = "INSERT INTO member VALUES (?, ?, ?, ?, ?)";
-		
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = DriverManager.getConnection(dbUrl, dbId, dbPw);
@@ -28,9 +28,9 @@ public class MemberDAO {
 			pstmt.setString(3, vo.getName());
 			pstmt.setString(4, vo.getTel());
 			pstmt.setString(5, vo.getRole().toString());
-			
+
 			pstmt.executeUpdate();
-			
+
 			pstmt.close();
 			conn.close();
 		} catch (ClassNotFoundException e) {
@@ -39,23 +39,23 @@ public class MemberDAO {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// id로 id만 조회
 	public MemberVO getIdById(String id) {
 		String sql = "SELECT id FROM member WHERE id = ?";
-		
+
 		MemberVO member = new MemberVO();
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = DriverManager.getConnection(dbUrl, dbId, dbPw);
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				member.setId(rs.getString("id"));
 			}
-			
+
 			rs.close();
 			pstmt.close();
 			conn.close();
@@ -64,14 +64,14 @@ public class MemberDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return member;
 	}
-	
+
 	// id, pw로 member 조회
 	public MemberVO getMemberByIdPw(MemberVO vo) {
 		String sql = "SELECT * FROM member WHERE id = ? and pw = ?";
-		
+
 		MemberVO member = new MemberVO();
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -79,20 +79,20 @@ public class MemberDAO {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, vo.getId());
 			pstmt.setString(2, vo.getPw());
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				member.setId(rs.getString("id"));
 				member.setPw(rs.getString("pw"));
 				member.setName(rs.getString("name"));
 				member.setTel(rs.getString("tel"));
-				if(rs.getString("rol").equals(Role.STAFF.name())) {
+				if (rs.getString("rol").equals(Role.STAFF.name())) {
 					member.setRole(Role.STAFF);
 				} else {
 					member.setRole(Role.CLIENT);
 				}
 			}
-			
+
 			rs.close();
 			pstmt.close();
 			conn.close();
@@ -101,39 +101,33 @@ public class MemberDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return member;
 	}
-	
+
 	public void revise(final String ID, final String PW, final String NAME, final String TEL) {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-		System.out.println("1. 드라이버 설정 성공.@@@@");
-		
-		
-		final String url = "jdbc:oracle:thin:@localhost:1521:xe";				;
-		final String user = "cafe";
-		final String password = "1234";
-		final Connection con = DriverManager.getConnection(url, user, password); 
-		System.out.println("2. db연결 성공.@@@@@");
-		
-		final String sql = "UPDATE member SET PW = ?, NAME = ?, TEL = ? WHERE ID = ?";
-		final PreparedStatement ps = con.prepareStatement(sql);
-		ps.setString(1, PW); 
-		ps.setString(2, NAME); 
-		ps.setString(3, TEL); 
-		ps.setString(4, ID); 
-		
-		System.out.println("3. sql문 생성.@@@@@@");
-		
-		ps.executeUpdate(); 
-		ps.close();
-		con.close();
+
+			final String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			;
+			final String user = "cafe";
+			final String password = "1234";
+			final Connection con = DriverManager.getConnection(url, user, password);
+
+			final String sql = "UPDATE member SET PW = ?, NAME = ?, TEL = ? WHERE ID = ?";
+			final PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, PW);
+			ps.setString(2, NAME);
+			ps.setString(3, TEL);
+			ps.setString(4, ID);
+
+			ps.executeUpdate();
+			ps.close();
+			con.close();
 		} catch (final Exception e) {
 			e.printStackTrace();
-		}		
-		
-		
-		
+		}
+
 	}//
 }
