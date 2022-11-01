079108e1145b0ac6069ece1fcf16a258e3c02fb9
KimHwiDong <ak7031@naver.com>
2022-11-01 16:20:09 +0900

    김휘동 버튼에 액션 이벤트 추가

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -39,7 +39,7 @@ public class Main {
 		btn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Page3 page = new Page3();
-//				page.open();
+				page.open();
 			}
 		});
 		btn3.setBounds(131, 230, 91, 23);
