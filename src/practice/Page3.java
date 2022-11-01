6833449ac0bace006ef7acd7605b232191960a95
KimHwiDong <ak7031@naver.com>
2022-11-01 19:49:56 +0900

    김휘동 / 주석 정리 및 버튼 이벤트에 카운트 추가

--- src/practice/Page3.java
+++ src/practice/Page3.java
@@ -1,18 +1,21 @@
 package practice;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 import dao.ItemDAO;
 import vo.ItemVO;
@@ -25,71 +28,61 @@ public class Page3 {
 	JPanel panel3 = new JPanel();
 	JTable table;
 	int i = 0;
+	int count = 0;
 
 	public void open() {
 		list = dao.list();
 		JFrame f = new JFrame();
 		f.setTitle("상품판매");
-		f.setSize(745, 900);
-//		f.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		f.setSize(745, 800);
 		f.getContentPane().add(panel1, BorderLayout.NORTH);
-		panel1.setPreferredSize(new Dimension(700, 300));
+		panel1.setPreferredSize(new Dimension(700, 500));
 		f.getContentPane().add(panel2, BorderLayout.CENTER);
 		f.getContentPane().add(panel3, BorderLayout.SOUTH);
-//		panel1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 
 		// table 항목명
-		String[] header = { "코드명", "가격", "이름"};
+		String[] header = { "코드명", "가격", "이름" };
 
 		Object[][] all = new Object[list.size()][3]; // 행, 열의 개수
-//		for (int i = 0; i < list.size(); i++) {
-//			ItemVO bag = list.get(i); // 가방을 1개 꺼내세요.
-//			all[i][0] = bag.getCode();
-//			all[i][1] = bag.getPrice();
-//			all[i][2] = bag.getName();
-//		}
 
 		table = new JTable(all, header); // 내용, 항목명
 		JScrollPane scrollPane = new JScrollPane(table);
-		scrollPane.setPreferredSize(new Dimension(600, 200));
+		scrollPane.setPreferredSize(new Dimension(600, 150));
 		panel2.add(scrollPane);
 
-		ArrayList<String> names = new ArrayList<>();
-		
+		Font font = new Font("굴림", Font.BOLD, 30);
+		JLabel lbl = new JLabel("주문 합계");
+		JTextField tf = new JTextField(10);
+		lbl.setFont(font);
+		tf.setFont(font);
+		tf.setBackground(Color.black);
+		tf.setForeground(Color.white);
+		tf.setHorizontalAlignment(JTextField.CENTER);
+		panel3.add(lbl);
+		panel3.add(tf);
+
 		for (i = 0; i < all.length; i++) {
 			JButton btn = new JButton(list.get(i).getName());
-			btn.setPreferredSize(new Dimension(150, 130));
-			names.add(list.get(i).getName());
+			btn.putClientProperty("id", i);
+			btn.setPreferredSize(new Dimension(150, 80));
 			// btn 이벤트
 			btn.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					int index = names.indexOf(e.getActionCommand());
-					all[index][0] = list.get(index).getCode();
-					all[index][1] = list.get(index).getPrice();
-					all[index][2] = list.get(index).getName();
-					System.out.println(all[index][0]);
-//						if (list.get(j).getName().equals("아메리카노")) {
-//							all[j][0] = list.get(j).getCode();
-//							System.out.println(list.get(j).getCode());
-//							break;
-//						} else {
-//							JOptionPane.showMessageDialog(table, "값이 없습니다.");
-//						}
-					
-					
+					int id = (Integer) btn.getClientProperty("id");
+					System.out.println(id);
+					all[id][0] = list.get(id).getCode();
+					all[id][1] = list.get(id).getPrice();
+					all[id][2] = list.get(id).getName();
+					count += (Integer) all[id][1];
+					tf.setText(count + "");
+					DefaultTableModel model = new DefaultTableModel(all, header);
+					table.setModel(model);
 				}
-			});
+			}); // event end
 			panel1.add(btn);
-		} //for end
+		} // for end
 
 		f.setVisible(true);
-
-		// test KimHwiDong
-
-		// 2022.10.31 AM 12:14 연습용 수정
-
-		// 2022.10.31 AM 12:25 연습용 수정_최종
-
 	}
 }
